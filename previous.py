7fa1a3097908ea9bf1260880c479db6033abeda3
kusti8 <kusti8@gmail.com>
2016-11-10 14:18:02 -0500

    Cleanup file opening

--- previous.py
+++ previous.py
@@ -2,13 +2,13 @@ import sys
 
 
 def get_colors(channel, changer):
-    prefs = open('/var/lib/hue-plus/previous').readlines()
-    if channel == 0:
-        return changer
-    elif channel == 1:
-        return (changer, prefs[1])  # Return the original one channel and the previous second channel
-    elif channel == 2:
-        return (prefs[0], changer)
+    with open('/var/lib/hue-plus/previous', 'r+') as prefs:
+        if channel == 0:
+            return changer
+        elif channel == 1:
+            return (changer, prefs[1])  # Return the original one channel and the previous second channel
+        elif channel == 2:
+            return (prefs[0], changer)
 
 if __name__ == '__main__':
     sys.exit(0)
